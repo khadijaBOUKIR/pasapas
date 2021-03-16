a57a0d5c5162110447786261b9f657476da54a0d
khadijaBOUKIR <kh2007.boukir@gmail.com>
2021-03-16 11:17:49 +0100

    pas a pas done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('header').load('./common/header.html');
-    $('section').load('./common/aside1.html');
-    
-
+    $('.container-aside1').load('./common/aside1.html');
+    $('.container-article').load('./common/article.html');
+    $('.container-aside2').load('./common/aside2.html');
+    $('footer').load('./common/footer.html');
 })
